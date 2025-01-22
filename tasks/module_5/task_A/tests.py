ff58a6cfea43bc0034e2699e88a1d25bbf5bd47a
Wiltort <wiltort21@gmail.com>
2025-01-22 19:32:08 +0500

    Tasks A B completed

--- tasks/module_5/task_A/tests.py
+++ tasks/module_5/task_A/tests.py
@@ -1,55 +1,36 @@
 import sys
 import unittest
-from io import StringIO
 import os
-from contextlib import contextmanager
-from unittest.mock import patch
-from random import choice, randint
-import string
+from random import randint
 
 
 class TestTaskSolution(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         print("-" * 70)
-        print("Тесты для задачи E (модуль 4)...")
-        sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..","..","app", "tests")))
-
+        print("Тесты для задачи A (модуль 5)...")
+        sys.path.append(
+            os.path.abspath(
+                os.path.join(
+                    os.path.dirname(__file__), "..", "..", "..", "app", "tests"
+                )
+            )
+        )
 
     def setUp(self):
         self.timeout = 1
-        self.solution_path = os.path.join("tasks", "module_4", "task_E", "solution.py")
-        
-
-    @contextmanager
-    def _run_with_input(self, input_text):
-        """Context manager to safely handle file operations and IO mocking"""
-        with open(self.solution_path, encoding='utf-8') as solution_file:
-            solution_code = solution_file.read()
-            with patch('sys.stdin', StringIO(input_text)), \
-                 patch('sys.stdout', new=StringIO()) as fake_output:
-                exec(solution_code)
-                yield fake_output.getvalue().strip()
-
-    def run_program_with_input(self, input_text):
-        """Helper method to run the program with specific input"""
-        with self._run_with_input(input_text) as output:
-            return output
 
     def test_random_10(self):
-        """Test with 10 random strings"""
+        """Test with 10 random pairs"""
         from timeout import run_with_timeout
-        characters = string.ascii_lowercase
-        n = 2000
+        from solution import rectangle_area
+
         for _ in range(10):
-            random_string = "".join(choice(characters) for i in range(n))
-            i = randint(0,n)
-            letter = choice(characters)
-            second_string = random_string[:i] + letter + random_string[i:]
-            input_string = random_string + '\n' + second_string
-            result = run_with_timeout(self.run_program_with_input, self.timeout, input_string)
+            pair = [randint(1, 10000) for i in range(2)]
+            area = pair[0] * pair[1]
+            result = run_with_timeout(rectangle_area, self.timeout, *pair)
             self.assertIsNotNone(result, msg="Время выполнения функции превышено!")
-            self.assertEqual(result, letter, msg="Неверный ответ")
+            self.assertEqual(result, area, msg="Неверный ответ")
 
 
 if __name__ == "__main__":
---
+++ tasks/module_5/task_B/tests.py
@@ -0,0 +1,45 @@
+import sys
+import unittest
+import os
+
+
+class TestTaskSolution(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print("-" * 70)
+        print("Тесты для задачи B (модуль 5)...")
+        sys.path.append(
+            os.path.abspath(
+                os.path.join(
+                    os.path.dirname(__file__), "..", "..", "..", "app", "tests"
+                )
+            )
+        )
+
+    def setUp(self):
+        self.timeout = 1
+
+    def test_pairs(self):
+        """Test with pairs"""
+        from timeout import run_with_timeout
+        from solution import snake_case
+
+        test_data = {
+            'Harry Potter and Germiona': 'harry_potter_and_germiona',
+            'For the Glory': 'for_the_glory',
+            'ARIA FOREVER': 'aria_forever',
+            'if You want': 'if_you_want',
+            'aA   hsjA': 'aa_hsja',
+            ' a you ready   ': 'a_you_ready',
+            'sDj': 'sdj',
+            'sto raz': 'sto_raz'
+        }
+
+        for key in test_data:
+            result = run_with_timeout(snake_case, self.timeout, key)
+            self.assertIsNotNone(result, msg="Время выполнения функции превышено!")
+            self.assertEqual(result, test_data[key], msg="Неверный ответ")
+
+
+if __name__ == "__main__":
+    unittest.main()
